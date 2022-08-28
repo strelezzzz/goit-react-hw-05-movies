99518db32c70c3d7dc85b78a7530ae678bc76f15
Taras Bondar <strelezzzz@gmail.com>
2022-08-28 23:14:12 +0300

    tranding-api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import TrandingMovies from "./TrandingMovies/TrandingMovies"
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <TrandingMovies/>
     </div>
   );
 };
--- src/services/API-service.js
+++ src/services/API-service.js
@@ -1,2 +1,33 @@
 // api key = b6c47595cd552e39159b1bc0cf721807
 // https://api.themoviedb.org/3/movie/550?api_key=b6c47595cd552e39159b1bc0cf721807
+
+// /trending/get-trending /trending/{media_type}/{time_window} список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+// /search/search-movies пошук фільму за ключовим словом на сторінці фільмів.
+// /movies/get-movie-details запит повної інформації про фільм для сторінки кінофільму.
+// /movies/get-movie-credits запит інформації про акторський склад для сторінки кінофільму.
+// /movies/get-movie-reviews запит оглядів для сторінки кінофільму.
+import axios from 'axios';
+const KEY = 'b6c47595cd552e39159b1bc0cf721807';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+async function getTrendingMovies() {
+  const response = await axios.get(`/trending/movie/day?api_key=${KEY} `);
+  if (response) {
+    const titles = await response.data.results.map(item => {
+      return {
+        title: item.title,
+        id: item.id,
+      };
+    });
+
+    return titles;
+  }
+  return await Promise.reject(new Error(`Somthing goes wrong`));
+}
+
+const api = {
+  getTrendingMovies,
+};
+
+export default api;
